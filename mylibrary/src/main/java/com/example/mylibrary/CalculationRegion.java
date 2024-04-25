268a88d2ecfe9bbbcb4d36500db9bc322795ab79
Sara Maria <82463650+sakazinha@users.noreply.github.com>
2024-04-24 21:56:34 -0300

    modificação library

--- mylibrary/src/main/java/com/example/mylibrary/CalculationRegion.java
+++ mylibrary/src/main/java/com/example/mylibrary/CalculationRegion.java
@@ -3,7 +3,7 @@ package com.example.mylibrary;
 public class CalculationRegion {
     // Método de Haversine para calcular a distância em metros entre duas coordenadas geográficas
     public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        final int R = 6371; // Raio da Terra em quilômetros
+        final int R = 6371; //Raio da Terra em quilômetros
 
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
@@ -14,7 +14,7 @@ public class CalculationRegion {
 
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        double distance = R * c * 1000; // Convertendo para metros
+        double distance = R * c * 1000; //Convertendo para metros
 
         distance = Math.pow(distance, 2) + Math.pow(0, 2);
 
